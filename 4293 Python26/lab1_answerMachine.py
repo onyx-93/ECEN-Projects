51ca486b7f1da239bbd3b7dad163bb4af0a1c1d6
onyx-93 <143036862+onyx-93@users.noreply.github.com>
2026-01-19 20:07:39 -0600

    finished

--- 4293 Python26/lab1_answerMachine.py
+++ 4293 Python26/lab1_answerMachine.py
@@ -1,17 +1,21 @@
-#Create a Python file in a code editor and write an "answering machine" program.
-#Inform the user who they're trying to reach.
-#Request the user's name, a contact method, and the reason for contacting you. Store these in separate variables.
-#Display the user's information on the screen.
-#(Bonus) Instead of displaying the user's information, write it to a file. Previous records should not be erased when running the program again.
 
-
+print("You have reached the answering machine for Daniel Dubon.")
 print('Enter name:', end= ' ')
 name = input()
 print('Enter contact information:', end= ' ')
 contact = input()
 print('Enter reason for contact:', end= ' ')
 reason = input()
-print('The following information was received:')
-print('The person who called was', name)
-print('Here is the contact information', contact)
-print('The reason was', reason)
+
+# Write information to a file (bonus)
+file = open("messages.txt", "a")
+
+file.write("New Message\n")
+file.write("Name: " + name + "\n")
+file.write("Contact Information: " + contact + "\n")
+file.write("Reason for Contact: " + reason + "\n")
+file.write("-" * 30 + "\n")
+
+file.close()
+
+print("\nYour message has been recorded. Thank you!")
