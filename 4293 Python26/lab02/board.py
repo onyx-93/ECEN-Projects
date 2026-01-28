23d29e9ff8d6ed12c90b75db07df3f1e39387188
onyx-93 <143036862+onyx-93@users.noreply.github.com>
2026-01-28 11:12:41 -0600

    win condition done

--- 4293 Python26/lab02/board.py
+++ 4293 Python26/lab02/board.py
@@ -7,22 +7,22 @@ class ConnectFourBoard:
 
     """Represents a Connect 4 board. Handles board state and checks moves for validity."""
 
-    def __init__(self, num_rows, num_cols): #constructor method
+    def __init__(self, num_rows, num_cols): #constructor method. self: the instance being created (passed automatically)
         """Initialize a new board"""
-        self.num_rows = num_rows
-        self.num_cols = num_cols
-        self.clear()
+        self.num_rows = num_rows #the number of rows in the board will be accessible anywhre in the class using self.num_rows
+        self.num_cols = num_cols #the number of columns in the board will be accessible anywhre in the class using self.num_cols
+        self.clear() #This will initialize the board grid with empty spaces (calls the instance method defined below).
 
     def clear(self): #instance method
         """Replace all pieces with empty spaces."""
-        self.rows = list()
-        for row in range(self.num_rows):
-            self.rows.append([_EMPTY for col in range(self.num_cols)])
+        self.rows = list() #Creates an empty list where self.rows can be used by other methods in the class.
+        for row in range(self.num_rows): # Loop that runs once for each row index from 0 to num_rows - 1.
+            self.rows.append([_EMPTY for col in range(self.num_cols)])# For each row, appends a list of _EMPTY strings representing empty cells in that row. each row is a list with num_cols empty spaces.
 
     def display(self): #instance method
         """Display the current board state"""
-        for row in range(self.num_rows):
-            print(f'\t|{"|".join(self.rows[row])}|')
+        for row in range(self.num_rows): # Loop that runs once for each row index from 0 to num_rows - 1.
+            print(f'\t|{"|".join(self.rows[row])}|') #Displays each row with '|' between each cell | | |X|O| | | |
         print('\t ' + ' '.join([str(col) for col in range(self.num_cols)]))
 
     def check_winner(self): #instance method
@@ -80,3 +80,5 @@ class ConnectFourBoard:
 
 # board = ConnectFourBoard(6, 7) this is an example of how to create an object of the class ConnectFourBoard
 # board.display() this is an example of how to call the display method on the object board 
+
+
