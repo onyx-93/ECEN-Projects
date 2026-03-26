9122377a1e95f148cb2edf6bd0629fffae37c3c8
landerich <ricland@okstate.edu>
2026-03-25 22:28:01 -0500

    Recursive FFT function made and finished

---
+++ 4293 Python26/Richie's Lab/Lab05/src/spectrogram.py
@@ -0,0 +1,55 @@
+from windowing import hann
+import numpy as np
+import matplotlib.pyplot as plt
+
+def fft_recursive(arr):
+    # Initialize array length and base case
+    N = len(arr)
+    if N == 1:
+        return np.array([arr[0]], dtype=complex)
+    # Split array into even and odd parts to start recursion
+    if N % 2 != 0:
+        raise ValueError("\n\tArray length must be a power of 2.\n")
+
+    X_even = fft_recursive(arr[0::2])
+    X_odd = fft_recursive(arr[1::2])
+
+    # Recombination of factors (Butterfly) step
+    X = np.zeros(N, dtype=complex)
+    for k in range(N//2):
+        multiplier = np.exp(-2j * np.pi * k/N)
+        p = X_even[k]
+        q = X_odd[k] * multiplier
+        X[k] = p + q
+        X[k + N//2] = p - q
+    return X
+
+if __name__ == "__main__":
+    # Generate a sample noise signal
+    t = np.arange(0.0, 20.5, 0.0005)
+    s1 = np.sin(2*np.pi*100*t)
+    s2 = 2*np.sin(2*np.pi*400*t)
+    s2[t <= 10] = s2[12 <= t] = 0
+    noise = 0.01*np.random.random(size=len(t))
+    x = s1 + s2 + noise
+    # TODO: Change this code to record an audio sample instead
+
+    # Iterate over the windowed audio and compute power spectrum data
+    psds = []
+    N = 1024 # samples per chunk of windowed audio
+    for chunk in hann(x, N):
+        # Compute the fast Fourier transform (FFT) of this chunk
+        X_full = fft_recursive(chunk)
+        X = X_full[0:N//2]
+        
+        # Compute the power spectral density (PSD) of this chunk
+        # PSD is 10*log10 of the square of the real part of the FFT
+        psd = 10*np.log10(np.abs(X)**2)
+        psds.append(psd)
+    psds = np.array(psds).transpose()
+
+    # Plot the PSDs as a spectrogram
+    # TODO: Add a subplot showing the interpolated audio waveform (with shared x-axis)
+    # TODO: Label and correct the x-axis and y-axis values
+    plt.imshow(psds, aspect='auto', origin='lower')
+    plt.show()
