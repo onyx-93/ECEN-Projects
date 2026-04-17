ca26e571153567a24cabf936417f5ad78029787d
landerich <ricland@okstate.edu>
2026-04-16 22:19:54 -0500

    Lab 6 Finished

--- 4293 Python26/Richie's Lab/Lab06/src/imgprocessfunc.py
+++ 4293 Python26/Richie's Lab/Lab06/src/imgprocessfunc.py
@@ -18,3 +18,23 @@ def edge_mask_gradient(grad: np.ndarray, threshold: float):
     mask = mag > threshold
     return mask
 
+def gradient_sobel_like(G: np.ndarray):
+    G = G.astype(float)
+    H, W = G.shape
+    grad = np.zeros((H, W, 2), dtype=float)
+
+    gx = (
+        -1*G[:-2, :-2] + 1*G[:-2, 2:] +
+        -2*G[1:-1, :-2] + 2*G[1:-1, 2:] +
+        -1*G[2:, :-2] + 1*G[2:, 2:]
+    ) / 8.0
+
+    gy = (
+        -1*G[:-2, :-2] - 2*G[:-2, 1:-1] - 1*G[:-2, 2:] +
+        1*G[2:, :-2] + 2*G[2:, 1:-1] + 1*G[2:, 2:]
+    ) / 8.0
+
+    grad[1:-1, 1:-1, 0] = gx
+    grad[1:-1, 1:-1, 1] = gy
+
+    return grad
